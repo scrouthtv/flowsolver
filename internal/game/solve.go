3d033020f9a60968678dc25846bff55eca20280f
Lenni <lennivh24@gmail.com>
2021-07-28 09:08:18 +0000

    basic solve tree

--- internal/game/solve.go
+++ internal/game/solve.go
@@ -1,5 +1,40 @@
 package game
 
+type SolveTreeNode struct {
+	x     int
+	y     int
+	dirs  map[Direction]SolveTreeNode
+	grown bool
+}
+
+type SolveTree struct {
+	root          SolveTreeNode
+	current       *SolveTreeNode
+	currDirection Direction
+}
+
 func (g *Game) AvailableAdvances(x int, y int) []Direction {
-	return nil
+	dirs := make([]Direction, 0)
+
+	for _, d := range AllDirections {
+		n := g.GetNeighbor(x, y, d)
+		if n != nil && *n == ColorNone {
+			dirs = append(dirs, d)
+		}
+	}
+
+	return dirs
+}
+
+func (g *Game) AvailableAdvancesN(x int, y int) int {
+	an := 0
+
+	for _, d := range AllDirections {
+		n := g.GetNeighbor(x, y, d)
+		if n != nil && *n == ColorNone {
+			an++
+		}
+	}
+
+	return an
 }
